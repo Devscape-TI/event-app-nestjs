a852873cd91b16a7b3838eb770bb3fd032963c5d
Filipe Costa <flp.costas@gmail.com>
2023-03-27 14:30:46 -0300

    change health check root path /

--- src/infra/http/default/app.controller.ts
+++ src/infra/http/default/app.controller.ts
@@ -14,7 +14,7 @@ export class AppController {
   @ApiForbiddenResponse({
     description: 'Response given when a command tries to break a domain rule',
   })
-  @Get('health')
+  @Get('/')
   health(): { health: boolean } {
     return { health: true };
   }
